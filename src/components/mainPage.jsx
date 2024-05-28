7db82405abd66153bd9abd7cb0819ef6fdd7539a
CohavDev <shaulspam@gmail.com>
2024-05-28 22:35:01 +0300

    update

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -5,35 +5,51 @@ import connectUnit from "../services/connectUnit";
 import { Button } from "@mui/joy";
 
 export default function MainPage(props) {
-  const [subscriber,setSubscriber] = useState(0)
-  const [license,setLicense] = useState(0)
-  const [innerId,setInnerId] = useState(0)
-  const [loading, setLoading] = useState(false)
-  const [serverMsgSuccess, setMsgSuccess] = useState("")
+  const [subscriber, setSubscriber] = useState(0);
+  const [license, setLicense] = useState(0);
+  const [innerId, setInnerId] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [serverMsgSuccess, setMsgSuccess] = useState("");
 
-  async function connectUnitHandleClick(){
-    const result = await connectUnit(subscriber,license,innerId,setLoading)
-    if(result){
+  async function connectUnitHandleClick() {
+    const result = await connectUnit(subscriber, license, innerId, setLoading);
+    if (result) {
       // const info = subscriber +", "+license +", "+innerId
-      setMsgSuccess(result)
+      setMsgSuccess(result);
     }
-
   }
   return (
     <Box>
-      <InputSection set={{subscriber:setSubscriber,license:setLicense,innerId:setInnerId}} />
+      <InputSection
+        set={{
+          subscriber: setSubscriber,
+          license: setLicense,
+          innerId: setInnerId,
+        }}
+      />
       <Box sx={{ paddingTop: 10 }}>
-        <Button  loading={loading} sx={{width: "25ch", m: 1 }}  onClick={()=>connectUnitHandleClick()}>
+        <Button
+          loading={loading}
+          sx={{ width: "25ch", m: 1 }}
+          onClick={() => connectUnitHandleClick()}
+        >
           צמד
         </Button>
         <Box sx={{ display: "flex", justifyContent: "space-between" }}>
           <Button variant="outlined">פתח שליחות</Button>
+
           <Button variant="outlined">החלף יחידה</Button>
         </Box>
       </Box>
-      {serverMsgSuccess !== "" && <Alert variant="outlined" severity="info" sx={{marginTop:5, backgroundColor:"white"}}>
-        {serverMsgSuccess}
-      </Alert>}
+      {serverMsgSuccess !== "" && (
+        <Alert
+          variant="outlined"
+          severity="info"
+          sx={{ marginTop: 5, backgroundColor: "white" }}
+        >
+          {serverMsgSuccess}
+        </Alert>
+      )}
     </Box>
   );
 }
